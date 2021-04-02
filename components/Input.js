240ece78c189bd963b60b171762ba690cfd7cadd
weshsnr <ndungugeoffrey11@gmail.com>
2021-04-02 23:08:26 +0300

    added a startGame screen, added color constants

---
+++ components/Input.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyleSheet, TextInput } from 'react-native'
+
+const Input = () => {
+    return (
+
+        <TextInput></TextInput>
+    )
+}
+
+export default Input
+
+const styles = StyleSheet.create({})
